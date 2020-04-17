ec5ad4935432d87377607c8fb5b46f6d3f05c3dd
yongui <ye2438@gmail.com>
2020-04-17 19:14:00 +0900

    finish store, getters lecture

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>Create Event</h1>
+  <div>
+    <h1>Create an Event, {{ user.name }}</h1>
+    <p>This event was created bu {{ user.id }}</p>
+    <span>total cat items : {{catLength}}</span>
+    <ul>
+      <li v-for="cat in categories" :key="cat.id">{{ cat }}</li>
+    </ul>
+  </div>
 </template>
+
+<script>
+import { mapState } from 'vuex'
+
+export default {
+  computed: {
+    catLength() {
+      return this.$store.getters.catLength
+    },
+    ...mapState(['user', 'categories'])
+  }
+}
+</script>
--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,16 +1,32 @@
 <template>
   <div>
     <h1>Events Listing</h1>
-    <EventCard/>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
 
 export default {
   components: {
     EventCard
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  created() {
+    EventService.getEvents()
+      .then(response => {
+        console.log(response.data)
+        this.events = response.data
+      })
+      .catch(error => {
+        console.log('there is an error : '+ error.response)
+      })
   }
 }
 </script>
